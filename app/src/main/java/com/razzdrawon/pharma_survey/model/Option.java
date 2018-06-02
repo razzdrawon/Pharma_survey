4083545d54b1ba1cf5bcdec4868124fe384fa1b9
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-06-01 19:56:55 -0500

    implementing basic data binding

---
+++ app/src/main/java/com/razzdrawon/pharma_survey/model/Option.java
@@ -0,0 +1,8 @@
+package com.razzdrawon.pharma_survey.model;
+
+/**
+ * Created by mapadi3 on 28/05/18.
+ */
+
+public class Option {
+}
---
+++ app/src/main/java/com/razzdrawon/pharma_survey/model/QuestionItem.java
@@ -0,0 +1,8 @@
+package com.razzdrawon.pharma_survey.model;
+
+/**
+ * Created by mapadi3 on 27/05/18.
+ */
+
+public class QuestionItem {
+}
