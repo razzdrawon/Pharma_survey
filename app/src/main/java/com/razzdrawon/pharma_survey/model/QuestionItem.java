9a0939e83b9350619827dab08beb5a0ec33387b3
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-06-01 19:58:17 -0500

    basic data binding

--- app/src/main/java/com/razzdrawon/pharma_survey/model/QuestionItem.java
+++ app/src/main/java/com/razzdrawon/pharma_survey/model/QuestionItem.java
@@ -1,8 +1,66 @@
 package com.razzdrawon.pharma_survey.model;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by mapadi3 on 27/05/18.
  */
 
-public class QuestionItem {
+public class QuestionItem implements Serializable {
+
+    private Integer qstnNbr;
+    private String qstnString;
+    private List<String> options;
+
+    public QuestionItem() {
+    }
+
+    public QuestionItem(Integer qstnNbr, String qstnString) {
+        this.qstnNbr = qstnNbr;
+        this.qstnString = qstnString;
+    }
+
+    public QuestionItem(Integer qstnNbr, String qstnString, List<String> options) {
+        this.qstnNbr = qstnNbr;
+        this.qstnString = qstnString;
+        this.options = options;
+    }
+
+    public Integer getQstnNbr() {
+        return qstnNbr;
+    }
+
+    public void setQstnNbr(Integer qstnNbr) {
+        this.qstnNbr = qstnNbr;
+    }
+
+    public String getQstnString() {
+        return qstnString;
+    }
+
+    public void setQstnString(String qstnString) {
+        this.qstnString = qstnString;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public String toViewString() {
+        return qstnNbr + ".- " + qstnString;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionItem{" +
+                "qstnNbr=" + qstnNbr +
+                ", qstnString='" + qstnString + '\'' +
+                ", options=" + options +
+                '}';
+    }
 }
