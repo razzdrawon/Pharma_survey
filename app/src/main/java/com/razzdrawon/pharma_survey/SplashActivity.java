c834691db7d6c256a79aad35897a65d4fc2f9279
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-06-19 20:09:38 -0500

    loginPage

--- app/src/main/java/com/razzdrawon/pharma_survey/SplashActivity.java
+++ app/src/main/java/com/razzdrawon/pharma_survey/SplashActivity.java
@@ -1,13 +1,35 @@
 package com.razzdrawon.pharma_survey;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int SPLASH_DELAY = 1500;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        setContentView(R.layout.activity_splash);
+        context = this;
+        showMainActivity();
+    }
+
+    private void showMainActivity() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent;
+                intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
     }
 }
--- app/src/main/java/com/razzdrawon/pharma_survey/model/LoginViewModel.java
+++ app/src/main/java/com/razzdrawon/pharma_survey/model/LoginViewModel.java
@@ -1,8 +1,33 @@
 package com.razzdrawon.pharma_survey.model;
 
-/**
- * Created by mapadi3 on 19/06/18.
- */
+import android.databinding.ObservableField;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 
 public class LoginViewModel {
+    public final ObservableField<String> user =
+            new ObservableField<>();
+    public final ObservableField<String> password =
+            new ObservableField<>();
+    public final ObservableField<Integer> busy =
+            new ObservableField<>();
+
+    public LoginViewModel(String user, String password) {
+        this.user.set(user);
+        this.password.set(password);
+        this.busy.set(View.GONE);
+    }
+
+    public void onClick(final View view) {
+        final String user = this.user.get();
+        final String password = this.password.get();
+        busy.set(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                busy.set(View.GONE);
+                Snackbar.make(view, user + ", " + password, Snackbar.LENGTH_SHORT).show();
+            }
+        }, 500);
+    }
 }
