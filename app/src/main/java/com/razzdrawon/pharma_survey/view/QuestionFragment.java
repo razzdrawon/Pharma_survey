1c499ea2d1652ef8caf3c128383e378c1ab1cd9b
Ricardo Axel Padilla Gonzalez <Ricardo.Padilla@walmart.com>
2018-06-23 19:10:02 -0500

    printing rb childs

--- app/src/main/java/com/razzdrawon/pharma_survey/view/QuestionFragment.java
+++ app/src/main/java/com/razzdrawon/pharma_survey/view/QuestionFragment.java
@@ -6,9 +6,12 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
+import com.razzdrawon.pharma_survey.R;
 import com.razzdrawon.pharma_survey.databinding.FragmentQuestionBinding;
 import com.razzdrawon.pharma_survey.model.Option;
 import com.razzdrawon.pharma_survey.model.QuestionItem;
@@ -36,14 +39,15 @@ public class QuestionFragment extends Fragment {
         options.add(new Option("c", "Option 3"));
 
         List<Option> optionsChild = new ArrayList<Option>();
-        options.add(new Option("x", "Option 1"));
-        options.add(new Option("y", "Option 2"));
-        options.add(new Option("z", "Option 3"));
-
+        optionsChild.add(new Option("x", "Option 1"));
+        optionsChild.add(new Option("y", "Option 2"));
+        optionsChild.add(new Option("z", "Option 3"));
 
         QuestionItem question = new QuestionItem(1, "Medicamentos prescritos por el serivio en el último trimestre (elija todas las que apliquen)", options);
 
-        createRadioButtons(question);
+        question.getOptions().get(0).setOptions(optionsChild);
+
+        createRadioButtons(question.getOptions(), mBinding.lyRadioGroup);
 
         mBinding.setQuestion(question);
 
@@ -51,21 +55,34 @@ public class QuestionFragment extends Fragment {
     }
 
 
-    private void createRadioButtons(QuestionItem question) {
+    private void createRadioButtons(List<Option> opts, LinearLayout layout) {
+        LinearLayout ll = new LinearLayout(getContext());
+        ll.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(30, 20, 30, 0);
+
         RadioGroup rg = new RadioGroup(getContext());
          //create the RadioGroup
         rg.setOrientation(RadioGroup.VERTICAL);//or RadioGroup.VERTICAL
-        int index = 0;
-        for(Option opt: question.getOptions()){
-
+        int position = 0;
+        for(Option opt: opts){
             RadioButton rb = new RadioButton(getContext());
-            rb.setText(opt.getOptString());
-            rb.setId(index);
+            rb.setText(opt.toViewString());
+            rb.setId(position);
             rb.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+            rb.setTag("");
+
+            if (opt.getOptions() != null && opt.getOptions().size() > 0) {
+                createRadioButtons(opt.getOptions(), ll);
+            }
+
             rg.addView(rb);
-            index++;
+            position++;
         }
-        mBinding.lyRadioGroup.addView(rg);
+        ll.addView(rg, layoutParams);
+
+        layout.addView(ll);
     }
 
 
